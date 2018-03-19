bf0c437fea68efd84e4a60d7acc004a0858a7237
ashleynicole472 <asha.4720@gmail.com>
2018-03-19 18:25:25 +0000

    Added edit, update, show and index listing

--- test_app/app/controllers/articles_controller.rb
+++ test_app/app/controllers/articles_controller.rb
@@ -1,6 +1,10 @@
 class ArticlesController < ApplicationController
     skip_before_action :verify_authenticity_token 
     
+  def index
+    @articles = Article.all
+  end
+    
   def new
     @article = Article.new
   end
@@ -9,10 +13,9 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     
     if @article.save
-      flash[:notice] = "Article was created successfully!"
       redirect_to articles_path(@article)
     else
-      render :new
+      render 'new'
     end
   end
   
@@ -20,8 +23,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
   
-  def index
-    @article = Article.all
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Successfully updated!"
+      redirect_to articles_path(@article)
+    else
+      render 'edit'
+    end
   end
   
   private
