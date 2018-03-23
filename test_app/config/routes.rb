b1d422dc943191ac084cb2f5db053ab1787bd409
ashleynicole472 <asha.4720@gmail.com>
2018-03-23 02:24:47 +0000

    Created new user sign up for UI

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :articles
-get 'welcome/home', to: 'welcome#home'
+  resources :articles
+  
+  get 'welcome/home', to: 'welcome#home'
+  
+  get 'signup', to: 'users#new'
+  
+  resources :users, except: [:new]
+  
 end
