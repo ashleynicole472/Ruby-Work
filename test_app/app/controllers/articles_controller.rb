f945eb7c0466feeb1a03af5c0bc2a29db4d70e30
ashleynicole472 <asha.4720@gmail.com>
2018-03-23 23:45:24 +0000

    Added paginate gem, added paginate to all articles

--- test_app/app/controllers/articles_controller.rb
+++ test_app/app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   skip_before_action :verify_authenticity_token 
     
   def index
-    @articles = Article.all
+    @articles = Article.paginate(page: params[:page], per_page: 5)
   end
     
   def new
