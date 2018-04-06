01b35da26aeeb1b2c262412f603f4f61e3b368ab
ashleynicole472 <asha.4720@gmail.com>
2018-04-06 23:05:36 +0000

    Fixed bug that wouldnt allow users to edit or delete their own posts

--- test_app/app/controllers/users_controller.rb
+++ test_app/app/controllers/users_controller.rb
@@ -60,14 +60,14 @@ class UsersController < ApplicationController
 end
 
   def require_same_user
-    if current_user != @user and !current_user.admin?
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "You can only edit your own account!"
       redirect_to articles_path
     end
   end
   
   def require_admin
-    if logged_in? and !current_user.admin?
+    if logged_in? && !current_user.admin?
       flash[:danger] = "Only admin users can delete perform this task!"
       redirect_to root_path
     end
