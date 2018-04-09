4ee408797f2f33c56fe0f70011f37e3bd3061c82
ashleynicole472 <asha.4720@gmail.com>
2018-04-09 18:12:39 +0000

    Attempting to fix admin issue

--- test_app/app/controllers/articles_controller.rb
+++ test_app/app/controllers/articles_controller.rb
@@ -67,7 +67,7 @@ class ArticlesController < ApplicationController
 end
 
   def require_same_user
-    if current_user != @article.user && !current_user.admin?
+    if current_user != @article.user || !current_user.admin?
       flash[:danger] = "You can only edit/delete your own articles"
       redirect_to articles_path
     end
