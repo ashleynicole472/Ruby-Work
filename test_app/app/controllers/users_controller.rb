64b45bdf5970a2d6f0cb923ce44323bb16f4621d
ashleynicole472 <asha.4720@gmail.com>
2018-04-09 18:08:41 +0000

    Attempting to fix admin user/user bug

--- test_app/app/controllers/users_controller.rb
+++ test_app/app/controllers/users_controller.rb
@@ -60,7 +60,7 @@ class UsersController < ApplicationController
 end
 
   def require_same_user
-    if current_user != @user && !current_user.admin?
+    if current_user != @user || !current_user.admin?
       flash[:danger] = "You can only edit your own account!"
       redirect_to articles_path
     end
